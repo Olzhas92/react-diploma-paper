1ad4b452a7aa88306c9c7f88b710436b0713293b
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-02-21 20:24:30 +0600

    TopSelling Section styling completed

--- src/components/Body/TopSelling/Cards/Europe.jsx
+++ src/components/Body/TopSelling/Cards/Europe.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "../TopSelling.module.css";
+import europe from "../../../../assets/europe.svg";
+import pin from "../../../../assets/pin.svg";
 
 export const Europe = () => {
   return (
-    <div>Europe</div>
-  )
-}
+    <div className={styles.europe}>
+      <img src={europe} alt="rome" className={styles.europe__image} />
+      <div className={styles.europe__destination}>
+        <span>Full Europe</span>
+        <span>$15k</span>
+      </div>
+      <div className={styles.europe__trip}>
+        <img src={pin} alt="" />
+        <span>28 Days Trip</span>
+      </div>
+    </div>
+  );
+};
--- src/components/Body/TopSelling/Cards/London.jsx
+++ src/components/Body/TopSelling/Cards/London.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import styles from "../TopSelling.module.css";
+import london from "../../../../assets/london.svg";
+import pin from "../../../../assets/pin.svg";
 
 export const London = () => {
-  return <div>London</div>;
+  return (
+    <div className={styles.london}>
+      <img src={london} alt="rome" className={styles.london__image} />
+      <div className={styles.london__destination}>
+        <span>London, UK</span>
+        <span>$$4.2k</span>
+      </div>
+      <div className={styles.london__trip}>
+        <img src={pin} alt="" />
+        <span>12 Days Trip</span>
+      </div>
+    </div>
+  );
 };
