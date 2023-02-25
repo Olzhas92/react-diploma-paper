a01455a86a1e7165219a9aac87815f00a3faf529
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-02-25 21:30:01 +0600

    Flights Page excluded

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Link } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Destinations } from "./pages/Destinations";
 import { Hotels } from "./pages/Hotels";
-import { Flights } from "./pages/Flights";
 import { Bookings } from "./pages/Bookings";
 import { PageNotFound } from "./pages/PageNotFound";
 import { Layout } from "./components/Layout/Layout";
@@ -17,7 +16,6 @@ const App = () => {
           <Route index element={<HomePage />}></Route>
           <Route path="destinations" element={<Destinations />} />
           <Route path="hotels" element={<Hotels />} />
-          <Route path="flights" element={<Flights />} />
           <Route path="bookings" element={<Bookings />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { HomePage } from "../../pages/HomePage";
 import { Registration } from "../Header/Registration";
 import jadoo from "../../assets/jadoo.svg";
 import styles from "./Navbar.module.css";
@@ -8,22 +7,15 @@ import styles from "./Navbar.module.css";
 export const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <Link to="/" element={<HomePage />}>
+      <Link to="/">
         <img src={jadoo} alt="" />
       </Link>
-
       <nav className={styles.nav}>
-        <Link to="/" className={styles.navlink}>
-          Home
-        </Link>
         <Link to="/destinations" className={styles.navlink}>
           Destinations
         </Link>
         <Link to="/hotels" className={styles.navlink}>
           Hotels
-        </Link>
-        <Link to="/flights" className={styles.navlink}>
-          Flights
         </Link>
         <Link to="/bookings" className={styles.navlink}>
           Bookings
