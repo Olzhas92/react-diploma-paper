cef922895e92ba41d15321ee29d7a93da38deeb0
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-03-10 21:54:22 +0600

    Bookings breakpoints styling completed

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import ritz from "../assets/ritz.jpg";
+import airplane from "../assets/Airplane.png";
 import styles from "./Bookings.module.css";
 
 export const Bookings = () => {
@@ -9,20 +11,23 @@ export const Bookings = () => {
   return (
     <div className={styles.bookings}>
       {hotelsArr.map((item) => (
-        <div key={item.id}>
-          <p>{item.hotel}</p>
-          <p>{item.price}</p>
-          <p>{item.city}</p>
+        <div key={item.id} className={styles.bookings__hotels}>
+          <img src={ritz} alt="ritz" />
+          <p>Hotel: {item.hotel}</p>
+          <p>Price: $ {item.price}</p>
+          <p>City: {item.city}</p>
         </div>
       ))}
       <br />
       <br />
       {flightsArr.map((item) => (
-        <div key={item.id}>
-          <p>{item.price}</p>
-          <p>{item.city}</p>
+        <div key={item.id} className={styles.bookings__flights}>
+          <img src={airplane} alt="airplane" />
+          <p>Price: $ {item.price}</p>
+          <p>City: {item.city}</p>
         </div>
       ))}
+      <p className={styles.total__price}>Total price:</p>
     </div>
   );
 };
